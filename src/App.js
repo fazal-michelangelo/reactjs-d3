73d759bda2373a26cc5d161e62bd7aa79ee203db
fazalhome <fazalzarif@gmail.com>
2020-04-22 22:58:08 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -8,9 +8,41 @@ import TimelineBubble from './LollipopChart3';
 import LollipopChartV1 from './LollipopChart5';
 import LollipopChartV2 from './LollipopChart6';
 import PerfectTimeline from './PerfectTimeline';
-import LineAreaChart from './LineAreaChart';
+import LineAreaChart from './LineAreaChart2';
   
 function App() {
+
+
+    var anyDataVal = [
+        {
+            color: "red",
+            value: "#f00"
+        },
+        {
+            color: "green",
+            value: "#0f0"
+        },
+        {
+            color: "blue",
+            value: "#00f"
+        },
+        {
+            color: "cyan",
+            value: "#0ff"
+        },
+        {
+            color: "magenta",
+            value: "#f0f"
+        },
+        {
+            color: "yellow",
+            value: "#ff0"
+        },
+        {
+            color: "black",
+            value: "#000"
+        }
+    ];
 
     return (
 
@@ -58,7 +90,7 @@ function App() {
                             <PerfectTimeline />
                         </Route>
                         <Route path="/line-area-chart">
-                            <LineAreaChart />
+                            <LineAreaChart getData={anyDataVal} />
                         </Route>                        
                         {/* <Route path="/perfect-timeline">
                             <SankeyChart />
